ba715fa146d7c1707af6d00a02ad62b20c3592d8
Clement <clement.blaise@gmail.com>
2015-01-25 14:27:55 +0100

    La camera est le cadavre

--- Assets/Script/Cadavre.cs
+++ Assets/Script/Cadavre.cs
@@ -9,27 +9,22 @@ public class Cadavre : MonoBehaviour {
 	{
 		Vector2 p = transform.position;
 		BoxCollider2D box = gameObject.GetComponent<BoxCollider2D> ();
-		Coins [0] = new Vector2 (p.x-(box.size.x), p.y-(box.size.y));
-		Coins [1] = new Vector2 (p.x+(box.size.x), p.y-(box.size.y));
-		Coins [2] = new Vector2 (p.x+(box.size.x), p.y+(box.size.y));
-		Coins [3] = new Vector2 (p.x-(box.size.x), p.y+(box.size.y));
-	
-		//Coins[0] = transform.rotation*(Coins[0]);
-		//Coins[1] = transform.rotation*(Coins[1]);
-		//Coins[2] = transform.rotation*(Coins[2]);
-		//Coins[3] = transform.rotation*(Coins[3]);
+		Coins [0] = new Vector2 (p.x-(box.size.x * transform.localScale.x/2), p.y-(box.size.y * transform.localScale.y/2));
+		Coins [1] = new Vector2 (p.x+(box.size.x * transform.localScale.x/2), p.y-(box.size.y * transform.localScale.y/2));
+		Coins [2] = new Vector2 (p.x+(box.size.x * transform.localScale.x/2), p.y+(box.size.y * transform.localScale.y/2));
+		Coins [3] = new Vector2 (p.x-(box.size.x * transform.localScale.x/2), p.y+(box.size.y * transform.localScale.y/2));
 
-//		GameObject[] points = new GameObject[4];
-//		points [0] = GameObject.Find ("Point1");
-//		points [1] = GameObject.Find ("Point2");
-//		points [2] = GameObject.Find ("Point3");
-//		points [3] = GameObject.Find ("Point4");
-//
-//		for(int i = 0; i < 4; i++)
-//		{
-//			Coins[i] = p+(Vector2)(transform.rotation*(Coins[i]-p));
-//			points[i].transform.position = Coins[i];
-//		}
+		GameObject[] points = new GameObject[4];
+		points [0] = GameObject.Find ("Point1");
+		points [1] = GameObject.Find ("Point2");
+		points [2] = GameObject.Find ("Point3");
+		points [3] = GameObject.Find ("Point4");
+
+		for(int i = 0; i < 4; i++)
+		{
+			Coins[i] = p+(Vector2)(transform.rotation*(Coins[i]-p));
+			points[i].transform.position = Coins[i];
+		}
 	}
 
 	void Start () {
